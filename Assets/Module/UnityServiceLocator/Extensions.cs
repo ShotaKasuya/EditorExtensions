86bd900e3d182dabd1fee9ca45bf12e4a711b46d
Kasuya <xiangtaipogu52@gmail.com>
2026-08-25 21:48:34 +0900

    public to internal

--- Assets/Module/UnityServiceLocator/Extensions.cs
+++ Assets/Module/UnityServiceLocator/Extensions.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 namespace Module.UnityServiceLocator
 {
-    public static class Extensions
+    internal static class Extensions
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T? OrNull<T>(this T obj) where T : Object?
+        internal static T? OrNull<T>(this T obj) where T : Object?
             => obj ? obj : null;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T AssertNull<T>(this T? reference) where T : class
+        internal static T AssertNull<T>(this T? reference) where T : class
         {
             Debug.Assert(reference != null);
             return reference!;
